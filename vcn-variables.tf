3458c467185fe5f103e876a1cea27b8ed4fd42d8
Yupei Yang <yupei.yang@oracle.com>
2021-11-04 16:44:09 -0700

    Add default value for vnc cidr block

--- vcn-variables.tf
+++ vcn-variables.tf
@@ -1,6 +1,7 @@
 variable "vcn_cidr_block" {
   type        = string
   description = "Primary VCN CIDR Block"
+  default     = "10.0.0.0/16"
 }
 
 variable "vcn_dns_label" {
@@ -18,22 +19,22 @@ variable "public_subnet_dns_label" {
   description = "Public Subnet DNS Label"
 }
 
-variable "private_subnet_cidr_block" {
+variable "private_subnet_cidr_blocks" {
   type        = list(string)
   description = "List of Private Subnet CIDR Block"
 }
 
-variable "private_subnet_dns_label" {
+variable "private_subnet_dns_labels" {
   type        = list(string)
   description = "List of Private Subnet DNS Label"
 }
 
-variable "database_subnet_cidr_block" {
+variable "database_subnet_cidr_blocks" {
   type        = list(string)
   description = "List of Database Subnet CIDR Block"
 }
 
-variable "database_subnet_dns_label" {
+variable "database_subnet_dns_labels" {
   type        = list(string)
   description = "List of Database Subnet DNS Label"
 }
