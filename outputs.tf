780de6e8c0ae9ad0f0449b0453bfda326d6cfb3c
Yupei Yang <yupei.yang@oracle.com>
2021-11-04 15:04:41 -0700

    Add outputs for vcn and subnets

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,31 @@ output "workload_compartment_ocids" {
   description = "Workload compartments to deploy applications to"
   value       = module.workload-compartment[*]
 }
+
+# -----------------------------------------------------------------------------
+# Output VCN and Subnets
+# -----------------------------------------------------------------------------
+output "vcn_ocid" {
+  description = "VCN ocid"
+  value       = module.vcn.vcn_id
+}
+
+output "public_subnet_id" {
+  description = "Public subnet ocid"
+  value       = module.vcn.public_subnet_id
+}
+
+output "private_subnet_id" {
+  description = "Private subnet ocid"
+  value       = module.vcn.private_subnet_id
+}
+
+output "database_subnet_id" {
+  description = "Database subnet ocid"
+  value       = module.vcn.database_subnet_id
+}
+
+output "fss_subnet_id" {
+  description = "Shared service subnet ocid"
+  value       = module.vcn.fss_subnet_id
+}
--- vcn/outputs.tf
+++ vcn/outputs.tf
@@ -1,3 +1,22 @@
+# -----------------------------------------------------------------------------
+# OCID Output
+# -----------------------------------------------------------------------------
 output "vcn_id" {
   value = oci_core_vcn.primary_vcn.id
+}
+
+output "public_subnet_id" {
+  value = oci_core_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = oci_core_subnet.private_subnet.*.id
+}
+
+output "database_subnet_id" {
+  value = oci_core_subnet.database_subnet.*.id
+}
+
+output "fss_subnet_id" {
+  value = oci_core_subnet.fss_subnet.id
 }
