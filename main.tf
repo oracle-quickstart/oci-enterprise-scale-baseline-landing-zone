8124ffc821db8b4c44731864bb6ebcd55e2aef96
sskarnaw <sakshi.karnawat@oracle.com>
2021-11-08 09:27:36 -0800

    Merging main

--- main.tf
+++ main.tf
@@ -88,6 +88,22 @@ module "workload-compartment" {
   depends_on = [ module.applications-compartment ]
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# Create IAM resources (policies, groups)
+# ---------------------------------------------------------------------------------------------------------------------
+module "iam" {
+  source                         = "./iam"
+  tenancy_ocid                   = var.tenancy_ocid
+  parent_compartment_id          = module.parent-compartment.parent_compartment_id
+  network_compartment_id         = module.network-compartment.network_compartment_id
+  network_compartment_name       = var.network_compartment_name
+  workload_compartment_name_list = var.workload_compartment_names
+  break_glass_username_list      = var.break_glass_username_list
+  depends_on = [
+    module.parent-compartment, module.network-compartment, module.workload-compartment
+  ]
+}
+
 # -----------------------------------------------------------------------------
 # Create VCN and subnets
 # -----------------------------------------------------------------------------
