e9d468675b0456651f58bf728fa056c562c90ade
Leo Siow <leo.siow@oracle.com>
2021-11-09 13:48:22 -0800

    clean up recipe code

--- security/main.tf
+++ security/main.tf
@@ -8,37 +8,6 @@ resource "oci_cloud_guard_detector_recipe" "cloud_guard_configuration_detector_r
     compartment_id = var.parent_compartment_ocid
     display_name   = var.configuration_detector_recipe_display_name
     source_detector_recipe_id = data.oci_cloud_guard_detector_recipes.configuration_detector_recipe.id
-
-    #Optional
-    description = var.detector_recipe_description
-    detector_rules {
-        #Required
-        details {
-            #Required
-            is_enabled = var.detector_recipe_detector_rules_details_is_enabled
-            risk_level = var.detector_recipe_detector_rules_details_risk_level
-
-            #Optional
-            condition = var.detector_recipe_detector_rules_details_condition
-            configurations {
-                #Required
-                config_key = var.detector_recipe_detector_rules_details_configurations_config_key
-                name = var.detector_recipe_detector_rules_details_configurations_name
-
-                #Optional
-                data_type = var.detector_recipe_detector_rules_details_configurations_data_type
-                value = var.detector_recipe_detector_rules_details_configurations_value
-                values {
-                    #Required
-                    list_type = var.detector_recipe_detector_rules_details_configurations_values_list_type
-                    managed_list_type = var.detector_recipe_detector_rules_details_configurations_values_managed_list_type
-                    value = var.detector_recipe_detector_rules_details_configurations_values_value
-                }
-            }
-            labels = var.detector_recipe_detector_rules_details_labels
-        }
-        detector_rule_id = oci_events_rule.test_rule.id
-    }
     freeform_tags = {
         "Description" = "Cloud guard configuration detector recipe"
         "CostCenter"  = var.tag_cost_center,
@@ -46,8 +15,18 @@ resource "oci_cloud_guard_detector_recipe" "cloud_guard_configuration_detector_r
     }
 }
 
+resource "oci_cloud_guard_detector_recipe" "cloud_guard_activity_detector_recipe" {
+    compartment_id = var.parent_compartment_ocid
+    display_name   = var.activity_detector_recipe_display_name
+    source_detector_recipe_id = data.oci_cloud_guard_detector_recipes.activity_detector_recipe.id
+    freeform_tags = {
+        "Description" = "Cloud guard activity detector recipe"
+        "CostCenter"  = var.tag_cost_center,
+        "GeoLocation" = var.tag_geo_location
+    }
+}
+
 resource "oci_identity_policy" "cloud_guard_policy" {
-    #Required
     compartment_id = var.parent_compartment_ocid
     description    = "OCI Landing Zone Cloud Guard Policy"
     name           = "Cloud Guard Policy"
@@ -73,48 +52,6 @@ resource "oci_identity_policy" "cloud_guard_policy" {
       "allow service cloudguard to read policies in tenancy ${var.parent_compartment_ocid}"
     ]
 
-    freeform_tags = {
-        "Description" = "Cloud guard activity detector recipe"
-        "CostCenter"  = var.tag_cost_center,
-        "GeoLocation" = var.tag_geo_location
-    }
-}
-
-resource "oci_cloud_guard_detector_recipe" "cloud_guard_activity_detector_recipe" {
-    compartment_id = var.parent_compartment_ocid
-    display_name   = var.activity_detector_recipe_display_name
-    source_detector_recipe_id = data.oci_cloud_guard_detector_recipes.activity_detector_recipe.id
-
-    #Optional
-    description = var.detector_recipe_description
-    detector_rules {
-        #Required
-        details {
-            #Required
-            is_enabled = var.detector_recipe_detector_rules_details_is_enabled
-            risk_level = var.detector_recipe_detector_rules_details_risk_level
-
-            #Optional
-            condition = var.detector_recipe_detector_rules_details_condition
-            configurations {
-                #Required
-                config_key = var.detector_recipe_detector_rules_details_configurations_config_key
-                name = var.detector_recipe_detector_rules_details_configurations_name
-
-                #Optional
-                data_type = var.detector_recipe_detector_rules_details_configurations_data_type
-                value = var.detector_recipe_detector_rules_details_configurations_value
-                values {
-                    #Required
-                    list_type = var.detector_recipe_detector_rules_details_configurations_values_list_type
-                    managed_list_type = var.detector_recipe_detector_rules_details_configurations_values_managed_list_type
-                    value = var.detector_recipe_detector_rules_details_configurations_values_value
-                }
-            }
-            labels = var.detector_recipe_detector_rules_details_labels
-        }
-        detector_rule_id = oci_events_rule.test_rule.id
-    }
     freeform_tags = {
         "Description" = "Cloud guard activity detector recipe"
         "CostCenter"  = var.tag_cost_center,
