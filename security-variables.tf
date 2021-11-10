7ddf8a72692ebc81d1b2da18d1f898b3c3bd2c77
Yupei Yang <yupei.yang@oracle.com>
2021-11-10 13:20:45 -0800

    Update description of cloud_guard_configuration_status

--- security-variables.tf
+++ security-variables.tf
@@ -1,6 +1,10 @@
 variable "cloud_guard_configuration_status" {
   type        = string
-  description = "the status of the Cloud Guard tenant"
+  description = "the status of the Cloud Guard tenant (ENABLED or DISABLED)"
+  validation {
+    condition     = var.cloud_guard_configuration_status == "ENABLED" || var.cloud_guard_configuration_status == "DISABLED"
+    error_message = "Please enter ENABLED or DISABLED for the status of the Cloud Guard Configuration."
+  }
 }
 
 variable "configuration_detector_recipe_display_name" {
