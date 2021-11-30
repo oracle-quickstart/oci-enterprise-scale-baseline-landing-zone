9045d491ec47f15b08018c85c20b1ca846c30213
Arjun Patel <arjun.patel@oracle.com>
2021-11-30 16:25:01 -0500

    CAAS-1190 added service connector and policy

--- security/flow-logs/main.tf
+++ security/flow-logs/main.tf
@@ -1,3 +1,7 @@
+resource "random_id" "policy_name" {
+  byte_length = 8
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Central logging group for VCN Flow Logs
 # ---------------------------------------------------------------------------------------------------------------------
@@ -16,7 +20,7 @@ resource "oci_logging_log_group" "central_log_group" {
 # VCN Flow Logs for each subnet in the Primary VCN
 # ---------------------------------------------------------------------------------------------------------------------
 resource "oci_logging_log" "vcn_flow_log" {
-  for_each     = {for subnet in data.oci_core_subnets.vcn_subnets.subnets: subnet.display_name => subnet.id}
+  for_each     = { for subnet in data.oci_core_subnets.vcn_subnets.subnets : subnet.display_name => subnet.id }
   display_name = "${var.log_display_name}-${each.key}"
   log_group_id = oci_logging_log_group.central_log_group.id
   log_type     = var.log_log_type
@@ -41,34 +45,56 @@ resource "oci_logging_log" "vcn_flow_log" {
   }
 }
 
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Service Connector policies
+# ---------------------------------------------------------------------------------------------------------------------
+# resource "oci_identity_policy" "log_analytics_policy" {
+#   compartment_id = var.security_compartment_ocid
+#   description    = "OCI Landing Zone Log Analytics Policy"
+#   name           = "${var.log_analytics_policy_name}-${random_id.policy_name.id}"
+
+#   freeform_tags = {
+#     "Description" = "Service Connector policy"
+#     "CostCenter"  = var.tag_cost_center,
+#     "GeoLocation" = var.tag_geo_location
+#   }
+
+#   statements = [
+#     "allow service loganalytics to {LOG_ANALYTICS_LIFECYCLE_INSPECT, LOG_ANALYTICS_LIFECYCLE_READ} in tenancy",
+#     "allow service loganalytics to INSPECT compartments in tenancy",
+#     "allow service loganalytics to USE tag-namespaces in tenancy",
+#     "allow service loganalytics to READ buckets compartment AppA",
+#     "allow service loganalytics to MANAGE cloudevents-rules in compartment AppA",
+#   ]
+# }
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Create Service Connector for VCN Flow Logs
 # ---------------------------------------------------------------------------------------------------------------------
-#  resource "oci_sch_service_connector" "vcn_flow_log_service_connector" {
-#    compartment_id = var.security_compartment_ocid
-#    display_name   = var.service_connector_display_name
-#    description    = "Service connector to transfer VCN Flow logs to Log Analytics"
+ resource "oci_sch_service_connector" "vcn_flow_log_service_connector" {
+   compartment_id = var.security_compartment_ocid
+   display_name   = var.service_connector_display_name
+   description    = "Service connector to transfer VCN Flow logs to Log Analytics"
 
-#    freeform_tags = {
-#      "Description" = "Service connector to transfer VCN flow log to Log Analytics",
-#      "CostCenter"  = var.tag_cost_center,
-#      "GeoLocation" = var.tag_geo_location
-#   }
-#
-#    source {
-#      kind = var.service_connector_source_kind
-#
-#      log_sources {
-#        compartment_id = var.parent_compartment_ocid
-#        log_group_id   = "_Audit"
-#      }
-#    }
-#
-#   target {
-#     kind                       = var.service_connector_target_kind
-#     batch_rollover_size_in_mbs = var.service_connector_target_batch_rollover_size_in_mbs
-#     batch_rollover_time_in_ms  = var.service_connector_target_batch_rollover_time_in_ms
-#     bucket                     = oci_objectstorage_bucket.audit_log_bucket.name
-#     namespace                  = data.oci_objectstorage_namespace.ns.namespace
-#   }
-# }
+   freeform_tags = {
+     "Description" = "Service connector to transfer VCN flow log to Log Analytics",
+     "CostCenter"  = var.tag_cost_center,
+     "GeoLocation" = var.tag_geo_location
+  }
+
+   source {
+     kind = var.service_connector_source_kind
+
+     log_sources {
+       compartment_id = var.security_compartment_ocid
+       log_group_id   = oci_logging_log_group.central_log_group.id
+     }
+   }
+
+  target {
+    kind                       = var.service_connector_target_kind
+    batch_rollover_size_in_mbs = var.service_connector_target_batch_rollover_size_in_mbs
+    batch_rollover_time_in_ms  = var.service_connector_target_batch_rollover_time_in_ms
+  }
+}
