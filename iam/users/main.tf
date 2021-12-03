0c0641efc284a23ba5c5f192bee3e4466b7b5c50
sskarnaw <sakshi.karnawat@oracle.com>
2021-12-03 08:40:07 -0800

    CAAS-1216: Adding changes to lift for each on the module

--- iam/users/main.tf
+++ iam/users/main.tf
@@ -3,11 +3,10 @@ resource "random_id" "user_name" {
 }
 
 resource "oci_identity_user" "break_glass_user" {
-    for_each = {for index, email in var.break_glass_user_email_list: index => email}
     compartment_id = var.tenancy_ocid
-    description    = "Break glass user ${each.key}"
-    name           = "break_glass_user_${each.key}_${random_id.user_name.hex}"
-    email          = each.value
+    description    = "Break glass user ${var.break_glass_user_index}"
+    name           = "break_glass_user_${var.break_glass_user_index}_${random_id.user_name.hex}"
+    email          = var.break_glass_user_email
     freeform_tags  = {
     "Description"  = "Break glass users",
     "CostCenter"   = var.tag_cost_center,
