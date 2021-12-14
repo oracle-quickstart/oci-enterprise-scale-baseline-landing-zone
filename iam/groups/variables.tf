4f08776c607dd16e3d0d070359d71dd46a679837
Arjun Patel <arjun.patel@oracle.com>
2021-12-14 13:03:40 -0500

    CAAS-1221 added option to overrride group names

--- iam/groups/variables.tf
+++ iam/groups/variables.tf
@@ -22,67 +22,56 @@ variable "workload_compartment_name_list" {
 variable "administrator_group_name" {
   type        = string
   description = "The name for the administrator group"
-  default     = "Administrators"
 }
 
 variable "network_admin_group_name" {
   type        = string
   description = "The name for the network administrator group name"
-  default     = "Virtual-Network-Admins"
 }
 
 variable "lb_users_group_name" {
   type        = string
   description = "The name for the load balancer users group name"
-  default     = "LBUsers"
 }
 
 variable "workload_storage_admins_group_name" {
   type        = string
   description = "The name for the workload storage administrators group"
-  default     = "Workload-Storage-Admins"
 }
 
 variable "workload_storage_users_group_name" {
   type        = string
   description = "The name for the workload storage users group"
-  default     = "Workload-Storage-Users"
 }
 
 variable "workload_admins_group_name" {
   type        = string
   description = "The name for the workload administrators group"
-  default     = "Workload-Admins"
 }
 
 variable "workload_users_group_name" {
   type        = string
   description = "The name for the workload users group"
-  default     = "Workload-Users"
 }
 
 variable "security_admins_group_name" {
   type        = string
   description = "The name of the security admin group"
-  default     = "SecurityAdmins"
 }
 
 variable "cloud_guard_operators_group_name" {
   type        = string
   description = "The name for the Cloud Guard Operator group name"
-  default     = "CloudGuard-Operator"
 }
 
 variable "cloud_guard_analysts_group_name" {
   type        = string
   description = "The name of the Cloud Guard Analyst group"
-  default     = "CloudGuard-Analyst"
 }
 
 variable "cloud_guard_architects_group_name" {
   type        = string
   description = "The name for the Cloud Guard Architect group name"
-  default     = "CloudGuard-Architect"
 }
 # -----------------------------------------------------------------------------
 # Tag Variables
