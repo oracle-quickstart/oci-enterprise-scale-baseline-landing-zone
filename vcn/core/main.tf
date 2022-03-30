46941bce25ae757f0b3ee9c893c5a0716d8d7e11
Arjun Patel <arjun.patel@oracle.com>
2022-03-30 16:18:54 -0400

    LZ-89 Removed the unlocked default SL

--- vcn/core/main.tf
+++ vcn/core/main.tf
@@ -230,55 +230,9 @@ resource "oci_core_drg_attachment" "drg_vcn_attachment" {
   }
 }
 
-# resource "oci_core_default_security_list" "lockdown" {
-#   manage_default_resource_id = oci_core_vcn.primary_vcn.default_security_list_id
-
-#   # count = var.lockdown_default_seclist == true ? 1 : 0
-# }
-
-resource "oci_core_default_security_list" "restore_default" {
-  // If variable is false, restore all default rules to default security list
+# -----------------------------------------------------------------------------
+# Lock Down Default Security List
+# -----------------------------------------------------------------------------
+resource "oci_core_default_security_list" "default_security_list_locked_down" {
   manage_default_resource_id = oci_core_vcn.primary_vcn.default_security_list_id
-
-  egress_security_rules {
-    // allow all egress traffic
-    destination = "0.0.0.0/0"
-    protocol    = "all"
-  }
-
-  ingress_security_rules {
-    // allow all SSH
-    protocol = "6"
-    source   = "0.0.0.0/0"
-    tcp_options {
-      min = 22
-      max = 22
-    }
-  }
-
-  ingress_security_rules {
-    // allow ICMP for all type 3 code 4
-    protocol = "1"
-    source   = "0.0.0.0/0"
-
-    icmp_options {
-      type = "3"
-      code = "4"
-    }
-  }
-
-  dynamic "ingress_security_rules" {
-    //allow all ICMP from all VCN CIDRs
-    for_each = oci_core_vcn.primary_vcn.cidr_blocks
-    iterator = vcn_cidr
-    content {
-      protocol = "1"
-      source   = vcn_cidr.value
-      icmp_options {
-        type = "3"
-      }
-    }
-  }
-
-  # count = var.lockdown_default_seclist == false ? 1 : 0
 }
