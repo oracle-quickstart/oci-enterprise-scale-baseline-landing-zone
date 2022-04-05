44ad877e4a6ff172a41cea1288e00f151ab1bd37
Yupei Yang <yupei.yang@oracle.com>
2022-04-05 14:13:55 -0700

    Add outputs.tf

---
+++ monitoring/notifications/outputs.tf
@@ -0,0 +1,4 @@
+output rules {
+  description = "Events rules"
+  value       = oci_events_rule.events_rules
+}
--- monitoring/topics/outputs.tf
+++ monitoring/topics/outputs.tf
@@ -1,5 +1,5 @@
 
 output "topics" {
-  description = "The topcs, indexed by keys in var.topics."
+  description = "The topics, indexed by keys in var.topics."
   value       = { for k, v in var.topics : k => oci_ons_notification_topic.topics[k] }
 } 
