a627300b0da405e9d920534398f57258edfb0962
Arjun Patel <arjun.patel@oracle.com>
2022-04-27 12:57:10 -0400

    switched to oracle/oci provider source and updated constraints in module

--- monitoring/tags/architecture-center/main.tf
+++ monitoring/tags/architecture-center/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    oci = {
+      source                = "oracle/oci"
+      configuration_aliases = [oci]
+    }
+  }
+}
+
 resource "random_id" "tag" {
   byte_length = 2
 }
--- monitoring/topics/security-topic/main.tf
+++ monitoring/topics/security-topic/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    oci = {
+      source                = "oracle/oci"
+      configuration_aliases = [oci]
+    }
+  }
+}
+
 resource "oci_ons_notification_topic" "security_topic" {
   compartment_id = var.compartment_id
   name           = "${var.security_topic_name}-${var.suffix}"
