08b32a11351e4b0f3c185c651c6bcd9b668682e6
Arjun Patel <arjun.patel@oracle.com>
2022-06-15 15:05:43 -0400

    LANZ-223 bug fix for outputs

--- vcn/core/outputs.tf
+++ vcn/core/outputs.tf
@@ -9,8 +9,8 @@ output "internet_gateway_id" {
   value = oci_core_internet_gateway.primary_internet_gateway.id
 }
 
-output "service_gateway_route_table" {
-  value = oci_core_route_table.service_gateway_route_table.id
+output "nat_gateway_id" {
+  value = oci_core_nat_gateway.nat_gateway.id
 }
 
 output "drg_id" {
