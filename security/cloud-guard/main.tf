8fa7195ca592d8478b2dbac4aaba4c73689ddccc
Yupei Yang <yupei.yang@oracle.com>
2022-06-27 10:36:34 -0700

    Add threat recipe and responder recipe

--- security/cloud-guard/main.tf
+++ security/cloud-guard/main.tf
@@ -79,5 +79,13 @@ resource "oci_cloud_guard_target" "cloud_guard_target" {
     detector_recipe_id = data.oci_cloud_guard_detector_recipes.activity_detector_recipe.detector_recipe_collection.0.items.0.id
   }
 
+  target_detector_recipes {
+    detector_recipe_id = data.oci_cloud_guard_detector_recipes.threat_detector_recipe.detector_recipe_collection.0.items.0.id
+  }
+
+  target_responder_recipes {
+    responder_recipe_id = data.oci_cloud_guard_responder_recipes.responder_recipe.responder_recipe_collection.0.items.0.id
+  }
+
   depends_on = [oci_cloud_guard_cloud_guard_configuration.tenancy_cloud_guard_configuration]
 }
