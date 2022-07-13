27c1276d54c2c8fd562188cc41168089deb8dc2d
Tierra Buissereth <tierra.b.buissereth@oracle.com>
2022-07-13 13:17:34 -0400

    Added budget recipients output

--- budget/outputs.tf
+++ budget/outputs.tf
@@ -10,6 +10,10 @@ output "budget_amount" {
   value = oci_budget_budget.oci_budget.amount
 }
 
-output "budget_alert_rule"{
+output "budget_alert_rule_threshold" {
   value = oci_budget_alert_rule.oci_budget_rule.threshold
+}
+
+output "budget_alert_rule_recipients" {
+  value = oci_budget_alert_rule.oci_budget_rule.recipients
 }
