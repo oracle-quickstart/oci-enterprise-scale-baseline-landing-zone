26760beeb4cbf0448bf2169fd9a50c7614c6cec0
Tierra Buissereth <tierra.b.buissereth@oracle.com>
2022-07-25 15:24:53 -0400

    Removed default budget variables

---
+++ budget/budget_tfvars.auto.tfvars
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------
+# Required variables
+# -----------------------------------------------------------------------------
+
+tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaxbchsnzhdxyoewmoqiqzvltba2ri7gijhbd2z5ybpgorv7yhxeeq"
+budget_amount = "100000"
+budget_target_name = "budget-test-target-name"
+budget_target = "ocid1.tenancy.oc1..aaaaaaaaxbchsnzhdxyoewmoqiqzvltba2ri7gijhbd2z5ybpgorv7yhxeeq"
+budget_alert_rule_threshold = "100"
+budget_alert_rule_recipients = "example3@test.com"
+tag_cost_center = "example_tag_cost_center"
+tag_geo_location = "example_tag_geo_location"
+
--- tests/test_budget.py
+++ tests/test_budget.py
@@ -2,9 +2,23 @@ from boto3 import Session
 from pytest_terraform import terraform
 
 import pytest
+import json
+import test_vars_helper
+
+data = {
+    "tenancy_ocid": "ocid1.tenancy.oc1..aaaaaaaaxbchsnzhdxyoewmoqiqzvltba2ri7gijhbd2z5ybpgorv7yhxeeq",
+    "budget_amount": "100000",
+    "budget_target_name": "budget-test-target-name",
+    "budget_target": "ocid1.tenancy.oc1..aaaaaaaaxbchsnzhdxyoewmoqiqzvltba2ri7gijhbd2z5ybpgorv7yhxeeq",
+    "budget_alert_rule_threshold": "100",
+    "budget_alert_rule_recipients": "example3@test.com",
+    "tag_cost_center": "example_tag_cost_center",
+    "tag_geo_location": "example_tag_geo_location",
+}
+
+test_vars_helper.create_var_files(data)
 
 #unit tests for budget module
-
 @pytest.mark.unit
 @terraform("budget", scope="session")
 def test_oci_budget_target_name(budget):
@@ -16,7 +30,7 @@ def test_oci_budget_target_name(budget):
 @terraform("budget", scope="session")
 def test_oci_budget_amount(budget):
     actual_budget_amount = budget["oci_budget_budget.oci_budget.amount"]
-    expected_budget_amount = 10000
+    expected_budget_amount = 100000
     assert actual_budget_amount == expected_budget_amount
 
 @pytest.mark.unit
