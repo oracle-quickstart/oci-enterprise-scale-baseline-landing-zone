c7c64377f47033dc27f39f61bb3d691f0ef019fa
Tierra Buissereth <tierra.b.buissereth@oracle.com>
2022-07-27 10:36:16 -0400

    [skip ci] removed unused imports

--- tests/test_budget.py
+++ tests/test_budget.py
@@ -1,20 +1,6 @@
-from boto3 import Session
 from pytest_terraform import terraform
 
 import pytest
-import json
-import test_vars_helper
-
-data = {
-    "tenancy_ocid": "ocid1.tenancy.oc1..aaaaaaaaxbchsnzhdxyoewmoqiqzvltba2ri7gijhbd2z5ybpgorv7yhxeeq",
-    "budget_amount": "100000",
-    "budget_target_name": "budget-test-target-name",
-    "budget_target": "ocid1.tenancy.oc1..aaaaaaaaxbchsnzhdxyoewmoqiqzvltba2ri7gijhbd2z5ybpgorv7yhxeeq",
-    "budget_alert_rule_threshold": "100",
-    "budget_alert_rule_recipients": "example3@test.com",
-    "tag_cost_center": "example_tag_cost_center",
-    "tag_geo_location": "example_tag_geo_location",
-}
 
 #unit tests for budget module
 @pytest.mark.unit
